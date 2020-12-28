642ce3333b32688a340b840a62fc060a36589161
htetshinelynn <htetshinelin.myanmar@gmail.com>
2020-12-28 10:18:01 +0630

    first Commit

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: Scaffold(
+        backgroundColor: Colors.green[400],
+        appBar: AppBar(
+          backgroundColor: Colors.lightBlue,
+          title: Text('Dice'),
+        ),
+        body: DiceApp(),
+      ),
+    );
+  }
+}
+
+class DiceApp extends StatefulWidget {
+  @override
+  _DiceAppState createState() => _DiceAppState();
+}
+
+class _DiceAppState extends State<DiceApp> {
+  int leftdiceresult = 1;
+  int rightdiceresult = 1;
+  void changedice() {
+    setState(() {
+      leftdiceresult = Random().nextInt(6) + 1;
+      rightdiceresult = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changedice();
+              },
+              child: Image.asset("images/dice$leftdiceresult.png"),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changedice();
+              },
+              child: Image.asset("images/dice$rightdiceresult.png"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
